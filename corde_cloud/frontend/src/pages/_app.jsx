f3de18442619e88f79b10c92bdecc6698b5f8ced
ueda masanari <Doraemon123453@gmail.com>
2023-05-12 10:35:36 +0900

    レスポンシブ対応完了

--- corde_cloud/frontend/src/pages/_app.jsx
+++ corde_cloud/frontend/src/pages/_app.jsx
@@ -1,5 +1,38 @@
-import '@/styles/globals.css'
+import { useEffect, useState } from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Styles from "@/styles/App.module.css";
+import "@/styles/globals.css"
+
+const GlobalStyle = createGlobalStyle`
+  // ここにグローバルなCSSを記述
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    const onResize = () => {
+      const targetScale = Math.min(
+        window.innerWidth / 414,
+        window.innerHeight / 896
+      );
+      setScale(targetScale);
+    };
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  });
+
+  return (
+    <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <div className={Styles.body}>
+        <div className={Styles.container} style={{ transform: `scale(${scale}) translate(-50%,-50%)` }}>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
