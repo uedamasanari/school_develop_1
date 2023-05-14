c83c3bbea233c43aecfdc4db639c7e6ffef2ae45
ueda masanari <Doraemon123453@gmail.com>
2023-05-15 00:39:53 +0900

    chatGPT接続

--- corde_cloud/frontend/src/pages/gpt.jsx
+++ corde_cloud/frontend/src/pages/gpt.jsx
@@ -1,9 +1,43 @@
-import styles from '@/styles/Gpt.module.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export default function Gpt() {
+const GptComponent = () => {
+  const [message, setMessage] = useState('');
+  const [input, setInput] = useState('');
+
+  const fetchMessage = async () => {
+    const configuration = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
+      }
+    };
+    const body = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+          {"role": "system", "content": "You are always angry but you always respond."},
+          {"role": "user", "content": input}
+      ]
+    };
+    
+    const res = await axios.post('https://api.openai.com/v1/chat/completions', body, configuration);
+    setMessage(res.data.choices[0].message.content);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchMessage();
+  }
+
   return (
-    <div className={styles.hello}>
-      hello!
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+      <p>{message}</p>
     </div>
-  )
+  );
 }
+
+export default GptComponent;
