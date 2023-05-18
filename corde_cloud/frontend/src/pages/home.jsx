cbfcc1a1808f6d3aa9971002fb07e15bf7be3abc
nanakoooooo <115598116+nanakoooooo@users.noreply.github.com>
2023-05-18 20:36:13 +0900

    a

--- corde_cloud/frontend/src/pages/home.jsx
+++ corde_cloud/frontend/src/pages/home.jsx
@@ -1,9 +1,10 @@
-import styles from '@/styles/Gpt.module.css'
+import React, { useState, useEffect } from 'react'
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.hello}>
-      あ
+    <div>
+      <p className={styles.tytle}>あなたへのおすすめ</p>
     </div>
   )
 }
