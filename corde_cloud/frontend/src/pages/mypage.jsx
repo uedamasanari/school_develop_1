8ac835bbb2cb4107abc96c5c50d2943fe7579238
owoodchucko <amu00934x0405@gmail.com>
2023-05-18 11:04:04 +0900

    a

--- corde_cloud/frontend/src/pages/mypage.jsx
+++ corde_cloud/frontend/src/pages/mypage.jsx
@@ -3,31 +3,32 @@ import styles from '@/styles/Mypage.module.css'
 export default function Mypage() {
 
   const MypageComponent = () =>{
+    const [user_picture,setUser_picture] =usestate("");
     const [user_name,setUser_name] =usestate("");
     const [mail_address,setMail_address] =usestate("");
-    const [profile_comment,setProfile_comment] =usestate("");
-  }
+
   
   return (
     <div>
       {/*ユーザー写真*/}
       
       {/* ユーザー名 */}
-      <p>{user_name}</p>
+      <p>ユーザー名</p>
 
       {/* メールアドレス */}
-      <p>{mail_address}</p>
+      <p>メールアドレス</p>
 
       {/* プロフィールコメント欄 */}
 
       {/* …続きを見る */}
 
       {/* プロフィールを編集 */}
-
-
+      <button className={styles.profile_setting_button} onClick={atackfunk}>プロフィールを編集</button>
 
       <img src='template/tag.svg' />
       <img src='template/add.svg' />
     </div>
   )
+
+  }
 }
