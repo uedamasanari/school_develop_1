a0df37d7fe239391dde046c101f863b9090883b7
owoodchucko <amu00934x0405@gmail.com>
2023-05-22 09:31:26 +0900

    a

--- corde_cloud/frontend/src/pages/setting.jsx
+++ corde_cloud/frontend/src/pages/setting.jsx
@@ -5,12 +5,24 @@ export default function Setting() {
   return (
 
 
-    <div>
+    <div className="main">
 
-          {/*タグイメージsvc*/}
+          {/*画像人間イメージsvc*/}
+          <img classNames={styles.picture_human_svc} src='setting/picture_human.svg'  width="50" height="50"/>
 
-          <img classNames={styles.add_svc} src='profile/add.svg' />
+          {/*画像人間に被せるカメライメージsvc*/}
+          <img classNames={styles.picture_camera_svc} src='setting/picture_camera.svg' />
 
+
+      {/* ユーザー名 */}
+      <p>ユーザー名</p>
+
+
+      {/* ユーザー名 */}
+      <p>メールアドレス</p>
+
+      {/*メールアドレスの横の鍵マークイメージsvc*/}
+      <img classNames={styles.add_svc} src='setting/Locked.svg' />
     </div>
 
 
