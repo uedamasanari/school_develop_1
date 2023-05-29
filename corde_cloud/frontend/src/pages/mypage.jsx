c73b3698e42b90c0fce0afb25d800feaed358707
owoodchucko <amu00934x0405@gmail.com>
2023-05-29 20:54:55 +0900

    a

--- corde_cloud/frontend/src/pages/mypage.jsx
+++ corde_cloud/frontend/src/pages/mypage.jsx
@@ -13,59 +13,84 @@ import styles from '@/styles/Mypage.module.css'
     const handleToggleExpand = () => {
       setExpanded(!expanded);
     };
- 
+
+
+  const insertLineBreaks = (text, maxCharacters) => {
+    const regex = new RegExp(`(.{1,${maxCharacters}})(\\s+|$)`, 'g');
+    return text.replace(regex, '$1\n');
+      };
 
 
   return (
     <div>
       <div className={styles.user_Profile}>
 
+        {/*ユーザー写真*/}
+        <div className={styles.image_container}>
+          <img className={styles.picture_human_svc} src='setting/picture_human.svg'/>
+        </div>
 
-      {/*ユーザー写真*/}
-      <div className={styles.image_container}>
-        <img className={styles.picture_human_svc} src='setting/picture_human.svg'/>
-      </div>
+        <div>
+          {/* ユーザー名 */}
+          <p className={styles.user_name}>ユーザー名</p>
 
-      <div>
-      {/* ユーザー名 */}
-      <p className={styles.user_name}>ユーザー名</p>
+          {/* メールアドレス */}
+          <p className={styles.user_mail}>メールアドレス</p>
 
-      {/* メールアドレス */}
-      <p className={styles.user_mail}>メールアドレス</p>
-
-      </div>
-
+        </div>
 
       </div>
+
+
+
 
       {/* プロフィールコメント欄 */}
       <p className={styles.user_comment}>プロフィールコメント欄</p>
 
-      <div className={`comment ${expanded ? 'expanded' : ''}`}>
 
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
+      <div className={`${styles.comment} ${expanded ? styles.expanded : ''}`}>
+        <div
+          className={styles.commentContent}
+          style={{
+            maxHeight: expanded ? 'none' : '2.4em',
+            overflow: 'hidden',
+            display: expanded ? 'block' : '-webkit-box',
+            WebkitLineClamp: expanded ? 'unset' : 2,
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
+          <p>{insertLineBreaks('コメントの内容きますああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ', 10)}</p>
+        </div>
       </div>
 
+
         {/* …続きを見る */}
+        {expanded ? (
         <div className={styles.read_more} onClick={handleToggleExpand}>
-          {expanded ? '閉じる' : '...続きを見る'}
+          閉じる
         </div>
+              ) : (
+        <div className={styles.read_more} onClick={handleToggleExpand}>
+        ...続きを見る
+        </div>
+        )}
+
+
+
+
 
       <div className={styles.main2}>
 
-      {/* プロフィールを編集 */}
-      <button className={styles.profile_setting_button}>プロフィールを編集</button>
+        {/* プロフィールを編集 */}
+        <button className={styles.profile_setting_button}>プロフィールを編集</button>
 
 
-      <div className={styles.box}>
+        <div className={styles.box}>
       </div>
 
 
-      {/*タグイメージsvc*/}
-      <img className={styles.tag_svc} src='profile/tag.svg' />
+        {/*タグイメージsvc*/}
+        <img className={styles.tag_svc} src='profile/tag.svg' />
 
 
 
