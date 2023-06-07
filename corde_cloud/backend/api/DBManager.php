9230d9991d08b615ea0c1afba394112e5648bb52
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-06-07 09:55:58 +0900

    DB接続関数(仮)
    仮です

---
+++ corde_cloud/backend/api/DBManager.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+class DBManager {
+    public function __construct() {
+            //$host = 'localhost';
+            //$dbname = 'your_database';
+            //$user = 'your_user';
+            //$password = 'your_password';
+
+            //try {
+               //$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+                //echo "success";
+            //} catch (PDOException $e) {
+                // 例外エラー
+                //echo "NO";
+                //throw $e;
+            //}
+            echo " aaa";
+        }
+}
+?>
--- corde_cloud/backend/api/hello.php
+++ corde_cloud/backend/api/hello.php
@@ -1,3 +1,4 @@
 <?php
-    echo "Hello World!";
+require_once 'DBManager.php';
+__construct();
 ?>
