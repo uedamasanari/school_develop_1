d62e6442906a3cfa67de22817bf791c0b4f63578
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-06-08 09:34:50 +0900

    検索作成途中

--- corde_cloud/backend/api/searchuser.php
+++ corde_cloud/backend/api/searchuser.php
@@ -2,29 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-class searchuser{
 require_once 'DBManager.php';
-public function searchInfo(){
-$dbManager = new DBManager();
-$dbManager->DBConect();
-$sql = "SELECT user_name, mail_address FROM USER_info";
-$result = $conn->query($sql);
-
-// 結果を配列に格納
-$data = array();
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $data[] = array(
-            'user_name' => $row['user_name'],
-            'mail_address' => $row['mail_address']
-        );
-    }
-}
-header('Content-Type: application/json');
-echo json_encode($data);
-}
-}
-
-
 
 ?>
