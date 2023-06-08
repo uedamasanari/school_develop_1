b483bfc3597959fd146372b85a962a8e10518608
kaito <2201113@s.asojuku.ac.jp>
2023-06-08 10:36:52 +0900

    postdetail

--- corde_cloud/frontend/src/pages/postdetails.jsx
+++ corde_cloud/frontend/src/pages/postdetails.jsx
@@ -10,9 +10,12 @@ const Postdetails = () => {
   const [likes, setLikes] = useState(0);
 
   const handleLikeClick = () => {
-      setIsLiked(!likes);
-      setLikes(likes ? likes - 1 : likes + 1);
-    };
+    setIsLiked(!likes);
+    setLikes(likes ? likes - 1 : likes + 1);
+  };
+  const handleBookClick = () => {
+    setIsBook(!books);
+  }
   return (
     <><MyHeader title={"詳細"} />
     <div className={styles.container}>
@@ -40,9 +43,11 @@ const Postdetails = () => {
             </svg>
           )}
         </span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className={`${styles.icon} ${styles.rightIcon}`} viewBox="0 0 16 16">
-          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
-        </svg>
+        <span onClick={handleBookClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className={`${styles.icon} ${styles.rightIcon}`} viewBox="0 0 16 16">
+            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
+          </svg>
+        </span>
       </div>
       <div>
         <hr className={styles.horizontalLine} />
