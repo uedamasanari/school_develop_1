22d7ad90fa984287e240d15b3de7d88417ec8658
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-06-08 09:58:42 +0900

    searchuserdata(仮)
    作成途中

--- corde_cloud/backend/api/hello.php
+++ corde_cloud/backend/api/hello.php
@@ -3,6 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 require_once 'DBManager.php';
+require_once 'searchuser.php';
 $dbManager = new DBManager();
 $dbManager->GetDB();
+$searcher = new searcheruser();
+$userData = $searcher->searchuser_info();
 ?>
--- corde_cloud/backend/api/searchuser.php
+++ corde_cloud/backend/api/searchuser.php
@@ -2,6 +2,32 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require_once 'DBManager.php';
 
+class searcheruser {
+    public function searchuser_info() {
+        $dbManager = new DBManager();
+        $pdo = $dbManager->GetDB(); // データベース接続を取得
+
+        $sql = "SELECT user_name, mail_address FROM USER_info";
+        $stmt = $pdo->query($sql);
+
+        // 結果を配列に格納
+        $data = array();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = array(
+                'user_name' => $row['user_name'],
+                'mail_address' => $row['mail_address']
+            );
+        }
+
+        return $data;
+    }
+}
+
+// searcheruserクラスのインスタンスを作成してメソッドを呼び出す例
+$searcher = new searcheruser();
+$userData = $searcher->searchuser_info();
+print_r($userData);
 ?>
