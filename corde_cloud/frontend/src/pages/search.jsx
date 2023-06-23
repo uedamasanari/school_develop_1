0007f3b936bd7e7b7f9e565134b149cfff9c07a3
nanakoooooo <115598116+nanakoooooo@users.noreply.github.com>
2023-06-23 10:36:54 +0900

    a

--- corde_cloud/frontend/src/pages/search.jsx
+++ corde_cloud/frontend/src/pages/search.jsx
@@ -1,24 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import styles from '@/styles/Search.module.css'
 
-export default function Home() {
+// export default function Home() {
+
+  const MyComponent = () => {
+
+    const linkClick = () =>{
+      window.location.href = '/home';
+    }
+
+    const handleLinkClick = () => {
+      window.location.href = '/searchResults';
+    }
+
+  
+
   return (
-    <div style={{backgroungimage:'url("search/closet.svg")'}}>
-        {/* <div className="container"> */}
+        <div className="container">
             <p className={styles.tagSearch}>タグ検索</p>
+
+            <div>
+              <a className={styles.plusbutton} onClick={linkClick}>+</a>
+            </div>
+            
+
                 <p className={styles.tag}>#季節</p>
                 <p className={styles.tag}>#柄</p>
                 <p className={styles.tag}>#色</p>
                 <p className={styles.tag}>#ブランド名  など</p>
-                <div className="button-container"><button className={styles.button}>表示</button></div>
-        {/* </div> */}
 
-        {/* <div className="container"> */}
+                {/* <div className="button-container"> */}
+                  {/* <button className={styles.button} onClick="goToLink(/searchResults)">検索</button> */}
+
+                  <div>
+                    <button className={styles.button} onClick={handleLinkClick}>検索</button>
+                  </div>
+                  
+                  
+        
+
+        
             <p className={styles.displayAll}>アイテムをすべて表示する</p>
-            <div className="button-container"><button className={styles.button}>表示</button></div>
-        {/* </div> */}
-
-    </div>
+                  <div>
+                    <button className={styles.button} onClick={handleLinkClick}>表示</button></div>
+                  </div>
     
   )
 }
+
+export default MyComponent;
+
+
+
--- corde_cloud/frontend/src/pages/searchResults.jsx
+++ corde_cloud/frontend/src/pages/searchResults.jsx
@@ -2,38 +2,46 @@ import React, { useState, useEffect } from 'react'
 import styles from '@/styles/Searchresults.module.css'
 
 export default function Home() {
+
+
+
   return (
     <div>
-        <div className="container">
-            <p className={styles.itemimg}>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-            </p>
-        </div>
+         <div className="container">
+             <p className={styles.itemimg}>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+             </p>
+         </div>
 
-        <div className="container">
-            <p className={styles.itemimg}>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-            </p>
-        </div>
+         <div className="container">
+             <p className={styles.itemimg}>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+             </p>
+         </div>
 
-        <div className="container">
-            <p className={styles.itemimg}>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-            </p>
-        </div>
+         <div className="container">
+             <p className={styles.itemimg}>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+             </p>
+         </div>
 
-        <div className="container">
-            <p className={styles.itemimg}>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-              <img src="searchResults/sample.jpeg" width="147" height="155"/>
-            </p>
-        </div>
+         <div className="container">
+             <p className={styles.itemimg}>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+               <img src="searchResults/sample.jpeg" width="147" height="155"/>
+             </p>
+         </div>
         
 
-    </div>
+     </div>
+    // <ul className = "bl_flexContainer">
+    //   <li className = "el_flexItem"></li>
+    //   <li className = "el_flexItem"></li>
+    //   <li className = "el_flexItem"></li>
+    // </ul>
     
   )
 }
