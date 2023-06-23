1b4177fbdb3f537913193fd334091fdaccddcf8b
kaito <2201113@s.asojuku.ac.jp>
2023-06-23 09:42:48 +0900

    timeline

--- corde_cloud/frontend/src/pages/timeline.jsx
+++ corde_cloud/frontend/src/pages/timeline.jsx
@@ -1,6 +1,7 @@
 import MyHeader from "@/components/MyHeader";
 import React, { useState } from 'react';
 import styles from '@/styles/Timeline.module.css';
+import axios from "axios";
 
 const Timeline = () =>  {
   const items = [1, 2, 3];
@@ -19,6 +20,18 @@ const Timeline = () =>  {
     setIsBooks(!books);
     setBooks(books ? books - 1 : books + 1);
   };
+  const handleGetLikesCount = (tweetId) => {
+  axios.get(`/code_cloud/backend/api/getTweetLikesCount.php?tweet_id=${tweetId}`)
+    .then(response => {
+      const likeCount = response.data.like_count;
+      // 取得したいいね数を利用する処理をここに書く
+    })
+    .catch(error => {
+      // エラーハンドリング
+      console.error(error);
+    });
+  };
+
   return (
     
     <div>
