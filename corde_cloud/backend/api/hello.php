633cebb2e84f499b59677a313af14aa11adecb4b
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-06-29 11:05:12 +0900

    aaaagff fgwg

--- corde_cloud/backend/api/hello.php
+++ corde_cloud/backend/api/hello.php
@@ -1,10 +1,1 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-require_once 'DBManager.php';
-require_once 'Insert.php';
-require_once 'Search_Info.php';
-$dbManager = new DBManager();
-$dbManager->GetDB();
-?>
+
