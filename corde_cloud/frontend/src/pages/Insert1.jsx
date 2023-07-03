5db17c7d91b3432bb348a6f351e2e707607e553d
2101033 <116693679+2101033@users.noreply.github.com>
2023-07-03 09:40:41 +0900

    reee

--- corde_cloud/frontend/src/pages/Insert1.jsx
+++ corde_cloud/frontend/src/pages/Insert1.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import styles from "@/styles/gpt/Insert.module.css"
 import axios from "axios";
+import MyHeader from "@/components/MyHeader";
 
 const Registration = () => {
   const [data, setData] = useState({
@@ -28,10 +30,25 @@ const Registration = () => {
       });
   };
 
+  //<MyHeader title={"新規登録"} />  一番上の新規登録のバナー
+  //import MyHeader from "@/components/MyHeader"; 新規登録のバナーを実装するときに必要なインポート
+
+  //<img src="Icon/Icon.png" alt="" className={styles.icon}/>　Iconの実装
+
+  //<div className={styles.shape}>　真ん中の四角を実装。
+
+  //それぞれに　className={styles.(CSSのクラス)}　を実装
+
+  
+
   return (
     <div>
+      <MyHeader title={"新規登録"} /> 
+      <img src="Icon/Icon.png" alt="" className={styles.icon}/>
       <form action="Insert.php" method="POST">
+      <div className={styles.shape}>
       <input
+        className={styles.input}
         type="text"
         placeholder="ユーザー名"
         name="user_name"
@@ -39,6 +56,7 @@ const Registration = () => {
         onChange={handleChange}
       />
       <input
+      className={styles.input}
         type="text"
         placeholder="コメント"
         name="user_coment"
@@ -46,6 +64,7 @@ const Registration = () => {
         onChange={handleChange}
       />
       <input
+      className={styles.input}
         type="email"
         placeholder="メールアドレス"
         name="mail_address"
@@ -53,13 +72,15 @@ const Registration = () => {
         onChange={handleChange}
       />
       <input
+      className={styles.input}
         type="password"
         placeholder="パスワード"
         name="password"
         value={data.password}
         onChange={handleChange}
       />
-      <button onClick={insertData}>登録</button>
+      <button className={styles.button} onClick={insertData}>登録</button>
+      </div>
     </form>
     </div>
   );
