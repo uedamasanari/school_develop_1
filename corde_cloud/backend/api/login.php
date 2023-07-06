fc2c85fc677f76eb957159905985d345af82d41e
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-07-06 10:32:02 +0900

    login関係

--- corde_cloud/backend/api/login.php
+++ corde_cloud/backend/api/login.php
@@ -3,35 +3,69 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 require_once 'DBManager.php';
-class Login{
-    function login($email, $password) {
-        $dbManager = new DBManager();
-        $pdo = $dbManager->GetDB(); // データベース接続を取得
-        if ($pdo) {
-            try {
-                $sql = "SELECT * FROM USER_info WHERE mail_address = :email";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':email', $email);
-                $stmt->execute();
-                $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                if ($user) {
-                    if (password_verify($password, $user['password'])) {
-                        // ログイン成功時の処理
-                        echo 'Login successful.';
-                        // 他の処理を追加する場合はここに記述します
-                    } else {
-                        // パスワードが一致しない場合の処理
-                        echo 'Invalid password.';
-                    }
+function login($email, $password) {
+    $dbManager = new DBManager();
+    $pdo = $dbManager->GetDB(); // データベース接続を取得
+    if ($pdo) {
+        try {
+            $sql = "SELECT * FROM USER_info WHERE mail_address = :email";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($user) {
+                $hashedPassword = $user['password'];
+                if (password_verify($password, $hashedPassword)) {
+                    // ログイン成功時の処理
+                    $response = [
+                        'status' => 'success',
+                        'message' => 'Login successful.'
+                    ];
                 } else {
-                    // ユーザーが存在しない場合の処理
-                    echo 'User not found.';
+                    // パスワードが一致しない場合の処理
+                    $response = [
+                        'status' => 'error',
+                        'message' => 'Invalid password.'
+                    ];
                 }
-            } catch (PDOException $e) {
-                echo 'Database error: ' . $e->getMessage();
+            } else {
+                // ユーザーが存在しない場合の処理
+                $response = [
+                    'status' => 'error',
+                    'message' => 'User not found.'
+                ];
             }
+        } catch (PDOException $e) {
+            // データベースエラーの場合の処理
+            $response = [
+                'status' => 'error',
+                'message' => 'Database error: ' . $e->getMessage()
+            ];
         }
+    } else {
+        // データベース接続エラーの場合の処理
+        $response = [
+            'status' => 'error',
+            'message' => 'Database connection error.'
+        ];
     }
+
+    // レスポンスをJSON形式で返す
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
+
+// POSTリクエストからパラメータを取得
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, true);
+$email = $input['email'];
+$password = $input['password'];
+
+// 入力されたパスワードをハッシュ化する
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+// ログイン処理の呼び出し
+login($email, $hashedPassword);
 ?>
