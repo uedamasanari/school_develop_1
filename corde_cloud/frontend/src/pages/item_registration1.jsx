b3793823c4eb2e844123893740e15faa6a8981f8
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-07-18 09:35:51 +0900

    qq

---
+++ corde_cloud/frontend/src/pages/item_registration1.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+const Registration = () => {
+  useEffect(() => {
+    const user_id = localStorage.getItem("user_id");
+  }, []);
+  const user_id = localStorage.getItem("user_item");
+  const [path, setpath] = useState();
+  const [item_name,setName] = userState();
+  const [loginStatus, setLoginStatus] = useState();
+  const [detail,setDetail] = userState();
+  const Login =() =>{
+    console.log(item_name,detail,user_id)
+    axios
+    .post("http://localhost:8000/api/Insert.php",{
+      item_name: item_name,
+      detail: detail,
+      user_id: user_id
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+  return(
+    <div>
+      <input
+        type="text"
+        placeholder="洋服詳細"
+        name="detail"
+        onChange={(e)=>setDetail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="パスワード"
+          name="password"
+        onChange={(e)=>setPass(e.target.value)}
+      />
+      <button onClick={Login}>ログイン</button>
+    </div>
+  );
+};
+export default Registration;
