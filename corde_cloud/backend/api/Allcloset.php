d6ab56f5ad9a1d090510eb953c0ca59bd4f5182f
Hikarri <115201211+Hikarri@users.noreply.github.com>
2023-07-18 10:43:00 +0900

    SQLファイル作成

--- corde_cloud/backend/api/Allcloset.php
+++ corde_cloud/backend/api/Allcloset.php
@@ -4,18 +4,20 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 require_once 'DBManager.php';
 
-function getCloset() {
+function getCloset($user_id) {
     $dbManager = new DBManager();
     $pdo = $dbManager->GetDB(); // データベース接続を取得
     if ($pdo) {
         try {
-            $sql = "SELECT * FROM closet";
+            $sql = "SELECT * FROM item WHERE user_id = :user_id";
             $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':user_id', $user_id);
             $stmt->execute();
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            $closet = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $response = [
                 'status' => 'success',
-                'message' => 'Login successful.'
+                'message' => 'Closet retrieval successful.',
+                'data' => $closet
             ];
         } catch (PDOException $e) {
             // データベースエラーの場合の処理
@@ -36,6 +38,10 @@ function getCloset() {
     header('Content-Type: application/json');
     echo json_encode($response);
 }
-
-getCloset($email, $password);
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, true);
+$user_id = $input['user_id'];
+// $user_id = 1;//仮置き1
+getCloset($user_id);
 ?>
+
--- corde_cloud/frontend/src/pages/item_registration1.jsx
+++ corde_cloud/frontend/src/pages/item_registration1.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 const Registration = () => {
   useEffect(() => {
-    const user_id = localStorage.getItem("user_id");
+    const user_id = window.localStorage.getItem("user_id");
   }, []);
-  const user_id = localStorage.getItem("user_item");
-  const [path, setpath] = useState();
+  const user_id = window.localStorage.getItem("user_item");
   const [item_name,setName] = useState();
-  const [loginStatus, setLoginStatus] = useState();
   const [detail,setDetail] = useState();
-  const Login =() =>{
-    console.log(item_name,detail,user_id)
+  const [image, setImage] = useState(null);
+  const handleImageChange = (e)=>{
+    if(e.target.files && e.target.files[0]){
+      setImage(e.target.files[0])
+    }
+  }
+  const insert =async (event) =>{
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('image',image);
+
     axios
-    .post("http://localhost:8000/api/Insert.php",{
-      item_name: item_name,
-      detail: detail,
-      user_id: user_id
+    .post("http://localhost:8000/api/Insert.php",formData,{
+      headers: {
+        'Content-Type':'multipart/form-data',
+      }
     })
     .then((response) => {
       console.log(response);
@@ -25,21 +32,22 @@ const Registration = () => {
     });
   };
   return(
-    <div>
+    <form onSubmit={insert}>
       <input
         type="text"
-        placeholder="洋服詳細"
+        placeholder="アイテム名"
         name="detail"
         onChange={(e)=>setDetail(e.target.value)}
         />
         <input
-          type="password"
-          placeholder="パスワード"
-          name="password"
-        onChange={(e)=>setPass(e.target.value)}
-      />
-      <button onClick={Login}>ログイン</button>
-    </div>
+        type="text"
+        placeholder="アイテム詳細"
+        name="detail"
+        onChange={(e)=>setDetail(e.target.value)}
+        />
+        <input type="file" accept="image/*" onChange={handleImageChange}/>
+      <button type="submit">登録</button>
+      </form>
   );
 };
 export default Registration;
