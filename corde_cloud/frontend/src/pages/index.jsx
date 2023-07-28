a987cce799c1b22e43199975f978ee1cb13b7482
ueda masanari <Doraemon123453@gmail.com>
2023-07-28 12:33:16 +0900

    Update index.jsx

--- corde_cloud/frontend/src/pages/index.jsx
+++ corde_cloud/frontend/src/pages/index.jsx
@@ -1,6 +1,11 @@
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
+useEffect(() => {
+  window.location.href = '/login';
+}, [])
 
 export default function Home() {
+
   return (
     <div>
       
